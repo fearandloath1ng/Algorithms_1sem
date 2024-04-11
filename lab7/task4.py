919244554e6a70306c424068eadf5177194da66f
fearandloath1ng <binoffd@gmail.com>
2024-04-11 20:23:11 +0300

    lab7 done

--- lab7/task4.py
+++ lab7/task4.py
@@ -18,6 +18,29 @@ import time
  
 t_start = time.perf_counter()
 
+def longest_common_subsequence(A, B):
+    n = len(A)
+    m = len(B)
+    dp = [[0] * (m + 1) for _ in range(n + 1)]
 
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            if A[i - 1] == B[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + 1
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+    return dp[n][m]
+
+with open('task4.txt', 'r') as file:
+    n = int(file.readline())
+    A = list(map(int, file.readline().split()))
+    m = int(file.readline())
+    B = list(map(int, file.readline().split()))
+
+p = longest_common_subsequence(A, B)
+
+with open('output.txt', 'w') as file:
+    file.write(str(p))
 
 print("Время работы: %s секунд " % (time.perf_counter() - t_start))
--- lab7/task5.py
+++ lab7/task5.py
@@ -21,6 +21,30 @@ import time
  
 t_start = time.perf_counter()
 
+def longest_common_subsequence(A, B, C):
+    n, m, l = len(A), len(B), len(C)
+    dp = [[[0 for _ in range(l+1)] for _ in range(m+1)] for _ in range(n+1)]
 
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            for k in range(1, l+1):
+                if A[i-1] == B[j-1] == C[k-1]:
+                    dp[i][j][k] = dp[i-1][j-1][k-1] + 1
+                else:
+                    dp[i][j][k] = max(dp[i-1][j][k], dp[i][j-1][k], dp[i][j][k-1])
+
+    return dp[n][m][l]
+
+with open("task5.txt", "r") as file:
+    n = int(file.readline())
+    A = list(map(int, file.readline().split()))
+    m = int(file.readline())
+    B = list(map(int, file.readline().split()))
+    l = int(file.readline())
+    C = list(map(int, file.readline().split()))
+
+result = longest_common_subsequence(A, B, C)
+with open("output.txt", "w") as file:
+    file.write(str(result))
 
 print("Время работы: %s секунд " % (time.perf_counter() - t_start))
